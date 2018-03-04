7326024f09ae51b0127fc92d79040e7b827c6c19
Paulo Schneider <paulo.schneider@gmail.com>
2018-03-04 18:24:28 +0000

    Add losing condition

--- breakout-with-phaser/game.js
+++ breakout-with-phaser/game.js
@@ -16,11 +16,17 @@ const create = () => {
   ball.anchor.set(0.5);
 
   game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.physics.arcade.checkCollision.down = false;
   game.physics.enable(ball, Phaser.Physics.ARCADE);
 
   ball.body.velocity.set(150, -150);
   ball.body.collideWorldBounds = true;
   ball.body.bounce.set(1);
+  ball.checkWorldBounds = true;
+  ball.events.onOutOfBounds.add(() => {
+    alert("Game over!");
+    location.reload();
+  }, this);
 
   paddle = game.add.sprite(game.world.width*0.5, game.world.height-5, "paddle");
   paddle.anchor.set(0.5,1);

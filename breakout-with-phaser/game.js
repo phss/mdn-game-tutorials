7903967d83489af55a0bffbbf69f53b838ce88e0
Paulo Schneider <paulo.schneider@gmail.com>
2018-03-04 18:22:15 +0000

    Add controlable paddle

--- breakout-with-phaser/game.js
+++ breakout-with-phaser/game.js
@@ -1,4 +1,5 @@
 var ball;
+var paddle;
 
 const preload = () => {
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -7,20 +8,31 @@ const preload = () => {
   game.stage.backgroundColor = "#eee";
 
   game.load.image("ball", "img/ball.png");
+  game.load.image("paddle", "img/paddle.png");
 };
 
 const create = () => {
-  ball = game.add.sprite(50, 50, "ball");
+  ball = game.add.sprite(game.world.width*0.5, game.world.height-25, "ball");
+  ball.anchor.set(0.5);
 
   game.physics.startSystem(Phaser.Physics.ARCADE);
   game.physics.enable(ball, Phaser.Physics.ARCADE);
 
-  ball.body.velocity.set(150, 150);
+  ball.body.velocity.set(150, -150);
   ball.body.collideWorldBounds = true;
   ball.body.bounce.set(1);
+
+  paddle = game.add.sprite(game.world.width*0.5, game.world.height-5, "paddle");
+  paddle.anchor.set(0.5,1);
+
+  game.physics.enable(paddle, Phaser.Physics.ARCADE);
+
+  paddle.body.immovable = true;
 };
 
 const update = () => {
+  game.physics.arcade.collide(ball, paddle);
+  paddle.x = game.input.x || game.world.width*0.5;
 };
 
 

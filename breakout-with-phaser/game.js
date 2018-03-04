a4da12dc4f87bd9668a685f2852e6be8f94bbc6e
Paulo Schneider <paulo.schneider@gmail.com>
2018-03-04 17:52:59 +0000

    Basic phaser setup

---
+++ breakout-with-phaser/game.js
@@ -0,0 +1,14 @@
+const preload = () => {
+
+};
+
+const create = () => {
+
+};
+
+const update = () => {
+
+};
+
+
+const game = new Phaser.Game(480, 320, Phaser.AUTO, null, { preload, create, update });

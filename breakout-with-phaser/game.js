3068cd14d24d5c126f9f36ea69861f2902c50814
Paulo Schneider <paulo.schneider@gmail.com>
2018-03-04 18:08:17 +0000

    Add correct phaser version and ball

--- breakout-with-phaser/game.js
+++ breakout-with-phaser/game.js
@@ -1,12 +1,16 @@
+var ball;
+
 const preload = () => {
   game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   game.scale.pageAlignHorizontally = true;
   game.scale.pageAlignVertically = true;
   game.stage.backgroundColor = "#eee";
-  console.log("herere");
+
+  game.load.image("ball", "img/ball.png");
 };
 
 const create = () => {
+  ball = game.add.sprite(50, 50, "ball");
 
 };
 

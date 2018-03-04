9fda6f09c11888581bd61cc6e36a9fc7868f48bb
Paulo Schneider <paulo.schneider@gmail.com>
2018-03-04 17:56:38 +0000

    Set scalling and background

--- breakout-with-phaser/game.js
+++ breakout-with-phaser/game.js
@@ -1,5 +1,9 @@
 const preload = () => {
-
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  game.scale.pageAlignHorizontally = true;
+  game.scale.pageAlignVertically = true;
+  game.stage.backgroundColor = "#eee";
+  console.log("herere");
 };
 
 const create = () => {
